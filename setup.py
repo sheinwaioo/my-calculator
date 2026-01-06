6afb992034cb610c8705423f9b9cfb22f007b5fb
SharliCain <s10269743@connect.np.edu.sg>
2026-01-06 14:35:33 +0800

    fix: correctly register src package for CI

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="calculator-app",
     version="0.1.0",
-    packages=find_packages(),
+    package_dir={"": "."},
+    packages=find_packages(where="."),
 )
